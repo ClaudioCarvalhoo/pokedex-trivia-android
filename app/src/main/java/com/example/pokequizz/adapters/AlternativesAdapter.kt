b90c282987485c6d7a08073a89afd2bbe282a1ba
Henrique Caúla <lhtc@cin.ufpe.br>
2019-12-14 03:28:55 -0300

    Renders question alternatives

---
+++ app/src/main/java/com/example/pokequizz/adapters/AlternativesAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.pokequizz.adapters
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import com.example.pokequizz.apiHelper.entities.Alternative
+import com.example.pokequizz.R
+import kotlinx.android.synthetic.main.alternative_item.view.alternative_title
+
+class AlternativesAdapter(private val context: Context,
+                          private val alternatives: List<Alternative>) : BaseAdapter() {
+
+    private val inflater: LayoutInflater
+            = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+    override fun getCount(): Int {
+        return alternatives.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return alternatives[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val rowView = inflater.inflate(R.layout.alternative_item, parent, false)
+
+        val alternative = alternatives[position]
+
+        Log.e("Alternative", alternative.text)
+
+        rowView.alternative_title.text = alternative.text
+
+        return rowView
+    }
+
+}
--- app/src/main/java/com/example/pokequizz/fragments/questions/QuestionsFragment.kt
+++ app/src/main/java/com/example/pokequizz/fragments/questions/QuestionsFragment.kt
@@ -4,18 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.pokequizz.apiHelper.entities.Question
 import com.example.pokequizz.R
+import com.example.pokequizz.activities.Questions
+import com.example.pokequizz.adapters.AlternativesAdapter
 import com.example.pokequizz.viewModels.QuestionsViewModel
 import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.questions_fragment.*
+import kotlinx.android.synthetic.main.questions_fragment.alternatives_list_view
+import kotlinx.android.synthetic.main.questions_fragment.imageView
+import kotlinx.android.synthetic.main.questions_fragment.question_title
 
 class QuestionsFragment : Fragment() {
 
     private lateinit var questionsViewModel: QuestionsViewModel
+    private lateinit var listView : ListView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,6 +32,10 @@ class QuestionsFragment : Fragment() {
 
         questionsViewModel.stem.observe(this, Observer<String> {
             question_title.text = it
+
+            listView = alternatives_list_view
+            val adapter = AlternativesAdapter(activity as Questions, question!!.alternatives)
+            listView.adapter = adapter
         })
         questionsViewModel.imageUrl.observe(this, Observer<String> {
             if (!it.isEmpty()) {
@@ -54,9 +64,12 @@ class QuestionsFragment : Fragment() {
     companion object {
         private const val ARG_STEM = "stem"
         private const val ARG_IMAGE_URL = "image_url"
+        private var question : Question? = null
 
         @JvmStatic
         fun newInstance(question: Question): QuestionsFragment {
+            this.question = question
+
             return QuestionsFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_STEM, question?.stem)
