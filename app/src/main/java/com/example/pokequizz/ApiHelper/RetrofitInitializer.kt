45b837da8d86cb1eed15f75b383c4b3719a4fd80
Henrique Caúla <lhtc@cin.ufpe.br>
2019-12-14 01:49:17 -0300

    Add question stem and image

--- app/src/main/java/com/example/pokequizz/ApiHelper/RetrofitInitializer.kt
+++ app/src/main/java/com/example/pokequizz/ApiHelper/RetrofitInitializer.kt
@@ -1,5 +1,7 @@
 package com.example.pokequizz.ApiHelper
 
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.GsonBuilder
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -7,9 +9,13 @@ class RetrofitInitializer {
 
     private val BASE_URL = "https://pokedex-trivia-api.herokuapp.com"
 
+    private val GSON = GsonBuilder()
+        .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+        .create()
+
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create())
+        .addConverterFactory(GsonConverterFactory.create(GSON))
         .build()
 
     fun apiHelper() = retrofit.create(ApiHelper::class.java)
